c1b7ab5a7e8d94ca1bf639a8c2943c1a321cfe20
Ammar Siddiqui <ammar.siddiqui847@gmail.com>
2020-07-14 15:37:15 -0400

    added closed and open node list

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ SCREEN = pygame.display.set_mode((WinX, WinY))
 CLOCK = pygame.time.Clock()
 SCREEN.fill((0,0,0))
 pygame.display.set_caption("A-star Pathfinder")
-run = True
-drag = False
 
 # Colours
 BLACK = (0, 0, 0)
@@ -21,12 +19,13 @@ RED = (255, 0, 0) # Analyzed Nodes
 
 # Classes
 class Node(object):
-    def __init__(self, x, y, barrier=False, start=False, end=False):
+    def __init__(self, x, y, barrier=False, start=False, end=False, closed=False):
         self.x = x
         self.y = y
         self.barrier = barrier
         self.start = start
         self.end = end
+        self.closed = closed
         self.fCost = None # gCost + hCost
         self.gCost = None # Distance from Current node
         self.hCost = None # Distance from End node
@@ -36,14 +35,30 @@ class Node(object):
         self.gCost = int(math.sqrt((self.x - current[0]) ** 2 + (self.y - current[1]) ** 2) * 10)
         self.hCost = int(math.sqrt((endX - current[0]) ** 2 + (endY - current[1]) ** 2) * 10)
         self.fCost = self.gCost + self.hCost
+    
+    def setSurrounding(self):
+        global closed_nodes, grid
+        if self.x > 0 and grid[self.y][self.x - 1].barrier == False:
+            closed_nodes.append((self.x - 1, self.y))
+        if self.x < len(grid[0]) - 1 and grid[self.y][self.x + 1].barrier == False:
+            closed_nodes.append((self.x + 1, self.y))
+        if self.y > 0 and grid[self.y - 1][self.x].barrier == False:
+            closed_nodes.append((self.x, self.y - 1))
+        if self.y < len(grid) - 1 and grid[self.y + 1][self.x].barrier == False:
+            closed_nodes.append((self.x, self.y + 1))
+
 
 # Variables
+run = True
+drag = False # Detecting Mouse Drag.
+algorithm = False # When the SPACE key is presssed and the algorithm has started.
 nodeSize = 20 # Pixel side length of each grid node.
 grid = [[Node(x, y) for x in range(WinX // 20)] for y in range(WinY // 20)]
 startX, startY = 24, 3
 endX, endY = 3, 17
 current = (startX, startY)
-analyzed = []
+closed_nodes = [] # nodes already evaluated.
+open_nodes = [] # nodes about to be evaluated.
 path = [] # x, y pairs of nodes that belong to the shortest path.
 grid[startY][startX].start = True
 grid[endY][endX].end = True
@@ -60,7 +75,7 @@ def setBarrier(x, y):
         grid[y][x].barrier = False
 
 def drawGrid():
-    global nodeSize
+    global nodeSize, startX, startY, endX, endY
     for n in grid:
         for Node in n:
             if Node.barrier == False:
@@ -77,23 +92,33 @@ def drawGrid():
             elif Node.barrier == True:
                 rect = pygame.Rect(Node.x * nodeSize, Node.y * nodeSize, nodeSize, nodeSize)
                 pygame.draw.rect(SCREEN, WHITE, rect, 0)
+    for i in closed_nodes:
+        rect = pygame.Rect(i[0] * nodeSize, i[1] * nodeSize, nodeSize, nodeSize)
+        pygame.draw.rect(SCREEN, RED, rect, 0)
 
 def drawPath():
+    global nodeSize
     for i in path: # i is a tuple
         rect = pygame.Rect(i[0] * nodeSize, i[1] * nodeSize, nodeSize, nodeSize)
         pygame.draw.rect(SCREEN, YELLOW, rect, 0)
 
 def analyze():
-    if current[0] = endX and current[1] = endY:
+    global current
+    if current[0] == endX and current[1] == endY:
         drawPath()
     else:
-        
+        print("analyzing")
+        grid[current[1]][current[0]].setSurrounding()
+        pygame.time.delay(150)
 
 # PyGame Loop
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        elif event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_SPACE: # Press "SPACE" key to start the Algorithm.
+                algorithm = True
         #elif event.type == pygame.KEYDOWN: 
             #if event.key == pygame.K_r: # Press "R" key to restart the program.
                 #grid = [[Node(x, y) for x in range(WinX // 20)] for y in range(WinY // 20)]
@@ -110,8 +135,9 @@ while run:
                 mouseX, mouseY = event.pos
                 setBarrier(mouseX, mouseY)
 
+    if algorithm:
+        analyze()
     drawGrid()
-
     pygame.display.update()
 
 if not run:
